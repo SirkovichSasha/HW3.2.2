d279e54a0f408f73dece75f70b542b07f0b20515
Sirkovich Sasha <sirkovichalexandra@gmail.com>
2020-05-14 15:42:19 +0300

    commit

--- app/src/main/java/com/example/calculatorhw312/MainActivity.java
+++ app/src/main/java/com/example/calculatorhw312/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.l);
 
         button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
