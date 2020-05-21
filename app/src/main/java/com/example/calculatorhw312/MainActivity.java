eba787257746113bedb08f4cba51cc8ebb0b16a1
Sirkovich Sasha <sirkovichalexandra@gmail.com>
2020-05-21 19:30:06 +0300

    commit

--- app/src/main/java/com/example/calculatorhw312/MainActivity.java
+++ app/src/main/java/com/example/calculatorhw312/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorhw312;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.l);
 
         button0 = (Button) findViewById(R.id.button0);
